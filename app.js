e6f2d1846d008ec4b19b4b6d633d68b9896ec1ad
juwan-25 <s2109@e-mirim.hs.kr>
2023-03-10 00:24:55 +0900

    서버에 html과 css 파일 적용해보기

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello world!");
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const fs = require('fs');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.use('/css', express.static('./static/css'));
+app.use('/js', express.static('./static/js'));
+
+app.get('/', function(request, response){
+    fs.readFile('./static/index.html', function(err, data){
+        if(err) {
+            response.send('에러');
+        } else {
+            response.writeHead(200, {'Content-Type':'text/html'}); // 헤더 작성
+            response.write(data); // html 데이터 보내기 
+            response.end(); // write 응답 시 end 필수
+        }
+    });
+});
+
+server.listen(323, function(){
+    console.log('서버 실행');
+});
