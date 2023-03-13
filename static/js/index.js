55395acd75e2ae768c7b2d86bf1f37396cdb61e1
juwan-25 <s2109@e-mirim.hs.kr>
2023-03-13 10:45:17 +0900

    채팅 기능 구현 완성 및 디자인

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,54 @@
-let socket = io();
+let socket = io()
 
 socket.on('connect', function() {
-    let name = prompt('반가워요!! 이름을 입력해주세요!', '');
-    if(!name){
-        name = '익명';
-    }
+  /* 이름 입력 */
+  let name = prompt('반갑습니다! 이름을 입력해주세요!!', '')
+  if(!name) {
+    name = '익명'
+  }
 
-    socket.emit('newUser', name);
-});
+  /* 새로운 유저 알림 */
+  socket.emit('newUser', name)
+})
 
-socket.on('update', function(data){
-    console.log(`${data.name}:${data.message}`);
-});
+/* 서버로부터 데이터 받은 경우 */
+socket.on('update', function(data) {
+  let chat = document.getElementById('chat')
 
+  let message = document.createElement('div')
+  let node = document.createTextNode(`${data.name} : ${data.message}`)
+  let className = ''
+
+  switch(data.type) {
+    case 'message':
+      className = 'other'
+      break
+
+    case 'connect':
+      className = 'connect'
+      break
+
+    case 'disconnect':
+      className = 'disconnect'
+      break
+  }
+
+  message.classList.add(className)
+  message.appendChild(node)
+  chat.appendChild(message)
+})
+
+/* 메시지 전송 함수 */
 function send() {
-    let message = document.getElementById('test').value;
-    document.getElementById('test').value = '';
-    socket.emit('message', {type : 'message', message: message});
-};
+  let message = document.getElementById('test').value
+  document.getElementById('test').value = ''
+
+  let chat = document.getElementById('chat')
+  let msg = document.createElement('div')
+  let node = document.createTextNode(message)
+  msg.classList.add('me')
+  msg.appendChild(node)
+  chat.appendChild(msg)
+
+  socket.emit('message', {type: 'message', message: message})
+}
